0c38ea6611c6a0594de18b625b03f208734b64a5
codejutsu1 <codejutsu@protonmail.com>
2024-02-03 20:38:11 +0100

    feat:added the task api resource and implemented all methods

---
+++ app/Http/Controllers/Task/TaskController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Task;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TaskResource;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): TaskResource
+    {
+        return TaskResource::collection(Task::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaskRequest $request): TaskResource
+    {
+
+        $task = Task::create($request->validated());
+
+        return new TaskResource($task);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task): TaskResource
+    {
+        return new TaskResource($task);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTaskRequest $request, Task $task): TaskResource
+    {
+        $task->update($request->validated());
+
+        return new TaskResource($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return response()->noContent();
+    }
+}
