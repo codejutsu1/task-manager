7bf4c5c67f8b833ad546538a4ad9c81ab8625a70
codejutsu1 <codejutsu@protonmail.com>
2024-02-04 20:35:17 +0100

    created the api route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Task\TaskController;
 
 /*
@@ -14,6 +15,8 @@ use App\Http\Controllers\Task\TaskController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
