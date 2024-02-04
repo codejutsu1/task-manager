042ee98806a5c54c48d3cfea45e888d28f20c1ae
codejutsu1 <codejutsu@protonmail.com>
2024-02-04 20:33:47 +0100

    implemented the login functionality

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StoreLoginRequest;
+
+class AuthController extends Controller
+{
+    public function login(StoreLoginRequest $request)
+    {
+        if(!Auth::attempt($request->validated())){
+            return response()->json([
+                'message' => 'Login Information invalid'
+            ], 401);
+        }
+
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        $token = $user->createToken('API Token of ' . $user->name)->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer'
+        ]);
+
+    }
+}
