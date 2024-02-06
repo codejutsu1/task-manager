082446c9103ad48dd96c08902715f3f254d790f3
codejutsu1 <codejutsu@protonmail.com>
2024-02-06 10:31:58 +0100

    got all users and prot protected the task route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Task\TaskController;
+use App\Http\Controllers\User\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,5 +23,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('auth:sanctum')->group(function() {
+    Route::apiResource('tasks', TaskController::class);
+});
 
-Route::apiResource('tasks', TaskController::class);
+Route::apiResource('users', UserController::class);
